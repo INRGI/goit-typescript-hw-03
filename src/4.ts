c074208e98d3012875bfe7dbdef000b13bd04fed
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-18 12:52:50 +0100

    Task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+};
+
+class Person {
+    private key: Key;
+    
+    constructor(key: Key) {
+        this.key;
+    }
+
+    getKey(): Key{
+        return this.key;
+    }
+};
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            console.log('Door is open');
+            this.tenants.push(person);
+        } else {
+            console.log('Door is closed');
+        }
+        
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Open door')
+        } else {
+            console.log('Wrong key')
+        }
+    }
+};
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
